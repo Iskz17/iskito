55866352de20f23c1709c79e12a4681c3942363d
Iskandar Roslen <61486047+Iskz17@users.noreply.github.com>
2023-05-27 20:26:33 +0800

    recovered

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Kito from "./kito-nobg.png";
 import ParticleBackground from "./particle";
 import NewmorphismBox from "./Neumorphism/NeumorphismBox";
 import GlassmorphismBox from "./Glassmorphism/GlassmorphismBox";
+import CommonDialog from "./Component/Dialog/CommonDialog";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import Button from "@mui/material/Button";
 import "./Particle.css";
+import AppTheme from "./Component/ThemeProvider/AppTheme";
 
 const App = () => {
   const headerItem = {
@@ -17,220 +22,277 @@ const App = () => {
     textAlign: "center",
   };
 
-  return (
-    <div className="App">
-      <div
-        id="parents div"
-        style={{
-          width: "100%",
-          height: "100vh",
-          position: "relative",
-          boxShadow: "0 70px 40px -40px rgba(142, 197, 252, 0.4)",
-          zIndex: 50,
-          fontSize: "2rem",
-        }}
-      >
-        <div
-          id="bubble"
-          style={{
-            position: "absolute",
-            // backgroundColor: "red",
-            width: "100%",
-            height: "100%",
-            overflow: "hidden",
-          }}
-        >
-          <ParticleBackground />
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div id="blurContainer"></div>
-        </div>
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-        <div
+  const [openDialogPicture, setOpenDialogPicture] = useState(false);
+  const handleCloseDialogPicture = () => {
+    setOpenDialogPicture(false);
+  };
+  const handleClickOpenDialogPicture = () => {
+    setOpenDialogPicture(true);
+  };
+
+  const [openDialogOne, setOpenDialogOne] = useState(false);
+  const handleCloseDialogOne = () => {
+    setOpenDialogOne(false);
+  };
+  const handleClickOpenDialogOne = () => {
+    setOpenDialogOne(true);
+  };
+
+  return (
+    <AppTheme>
+      <div className="App">
+        {/* <div
+          id="parents div"
           style={{
-            position: "absolute",
             width: "100%",
-            height: "100%",
-            fontSize: "1em",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            color: "grey",
-            paddingLeft: "10%",
-            paddingRight: "10%",
-            fontWeight: 700,
-            //background: "red",
+            height: "100vh",
+            position: "relative",
+            boxShadow: "0 70px 40px -40px rgba(142, 197, 252, 0.4)",
+            zIndex: 50,
+            fontSize: "2rem",
           }}
         >
-          <div className="descriptionDiv">
-            <span id="intro" className="introFont">{`Hi, I'm {Iskandar}`}</span>
-            <div style={{ marginTop: "10px" }}></div>
-            <span style={{ fontSize: "1em" }}>
-              <span style={{ fontSize: "0.8em" }}>
-                <Typewriter
-                  options={{
-                    autoStart: true,
-                    loop: true,
-                  }}
-                  style={{ marginTop: "1000px" }}
-                  onInit={(typewriter) => {
-                    typewriter
-                      .typeString("I'm a web developer from Malaysia 😁")
-                      .pauseFor(1000)
-                      .typeString("and currently based in Malaysia")
-                      .deleteAll()
-                      .typeString(
-                        "I have an experience in {C# .net} and {React ⚛️}!! 🛠️🚀 "
-                      )
-                      .pauseFor(1000)
-                      .deleteAll()
-                      .typeString("This page is for experiment ⚗️🧪")
-                      .pauseFor(1000)
-                      .typeString("and also keeping important notes ✍️🗒️")
-                      .pauseFor(1000)
-                      .deleteAll()
-                      .start();
-                  }}
-                />
-              </span>
-            </span>
+          <div
+            id="bubble"
+            style={{
+              position: "absolute",
+              // backgroundColor: "red",
+              width: "100%",
+              height: "100%",
+              overflow: "hidden",
+            }}
+          >
+            <ParticleBackground />
           </div>
-          <div className="pictureDiv">
-            <img
-              src={Kito}
-              alt="profile pic"
-              style={{
-                width: "23rem",
-                objectFit: "cover",
-                position: "absolute",
-                filter: "drop-shadow(0 30px 0.75rem rgba(0,0,0,0.3))",
-                transition: ".5s ease",
-              }}
-            />
-          </div>
-        </div>
-        <div
-          id="header"
-          style={{
-            position: "fixed",
-            width: "100%",
-            height: "55px",
-            boxShadow: "0px 30px 50px -20px rgba(0,0,0,0.5)",
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            paddingRight: "10%",
-          }}
-        >
-          <div style={{ ...headerItem }}>Home</div>
-          <div style={{ ...headerItem }}>Profile</div>
-          <div style={{ ...headerItem }}>Notes</div>
-        </div>
-        <div
-          style={{
-            width: "80px",
-            marginLeft: "40px",
-            //background: "black",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            flexWrap: "wrap",
-            position: "absolute",
-            zIndex: 10000,
-          }}
-        >
-          <div
-            style={{
-              background: "red",
-              width: "100%",
-              height: "50px",
-              marginBottom: "5px",
-            }}
-            onClick={() => {
-              alert("first is being clicked");
-            }}
-          ></div>
-          <div
-            style={{
-              background: "red",
-              width: "100%",
-              height: "50px",
-              marginBottom: "5px",
-            }}
-          ></div>
-          <div
-            style={{
-              background: "red",
-              width: "100%",
-              height: "50px",
-              marginBottom: "5px",
-            }}
-          ></div>
-          <div
-            style={{
-              background: "red",
-              width: "100%",
-              height: "50px",
-              marginBottom: "5px",
-            }}
-          ></div>
-        </div>
-      </div>
-      <div
-        id="parents div"
-        style={{
-          width: "100%",
-          height: "100vh",
-          background: "white",
-        }}
-      >
-        <NewmorphismBox />
-      </div>
-      <div
-        id="parents div"
-        style={{
-          width: "100%",
-          height: "100vh",
-          background: "white",
-        }}
-      >
-        <div
-          id="bubble"
-          style={{
-            position: "absolute",
-            // backgroundColor: "red",
-            width: "100%",
-            height: "100%",
-            overflow: "hidden",
-          }}
-        >
-          <ParticleBackground />
           <div
             style={{
               position: "absolute",
-              zIndex: 100,
               width: "100%",
               height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <GlassmorphismBox />
+            <div id="blurContainer"></div>
           </div>
+
+          <div
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              fontSize: "1em",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              color: "grey",
+              paddingLeft: "10%",
+              paddingRight: "10%",
+              fontWeight: 700,
+              //background: "red",
+            }}
+          >
+            <div className="descriptionDiv">
+              <span
+                id="intro"
+                className="introFont"
+              >{`Hi, I'm {Iskandar}`}</span>
+              <div style={{ marginTop: "10px" }}></div>
+              <span style={{ fontSize: "1em" }}>
+                <span style={{ fontSize: "0.8em" }}>
+                  <Typewriter
+                    options={{
+                      autoStart: true,
+                      loop: true,
+                    }}
+                    style={{ marginTop: "1000px" }}
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString("I'm a web developer from Malaysia 😁")
+                        .pauseFor(1000)
+                        .typeString("and currently based in Malaysia")
+                        .deleteAll()
+                        .typeString(
+                          "I have an experience in {C# .net} and {React ⚛️}!! 🛠️🚀 "
+                        )
+                        .pauseFor(1000)
+                        .deleteAll()
+                        .typeString("This page is for experiment ⚗️🧪")
+                        .pauseFor(1000)
+                        .typeString("and also keeping important notes ✍️🗒️")
+                        .pauseFor(1000)
+                        .deleteAll()
+                        .start();
+                    }}
+                  />
+                </span>
+              </span>
+            </div>
+            <div className="pictureDiv">
+              <img
+                src={Kito}
+                alt="profile pic"
+                style={{
+                  width: "23rem",
+                  objectFit: "cover",
+                  position: "absolute",
+                  filter: "drop-shadow(0 30px 0.75rem rgba(0,0,0,0.3))",
+                  transition: ".5s ease",
+                }}
+              />
+            </div>
+          </div>
+          <div
+            id="header"
+            style={{
+              position: "fixed",
+              width: "100%",
+              height: "55px",
+              boxShadow: "0px 30px 50px -20px rgba(0,0,0,0.5)",
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              paddingRight: "10%",
+            }}
+          >
+            <div style={{ ...headerItem }}>Home</div>
+            <div style={{ ...headerItem }}>Profile</div>
+            <div style={{ ...headerItem }}>Notes</div>
+          </div>
+          <div
+            style={{
+              width: "80px",
+              marginLeft: "40px",
+              //background: "black",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              flexWrap: "wrap",
+              position: "absolute",
+              zIndex: 10000,
+            }}
+          >
+            <div
+              style={{
+                background: "red",
+                width: "100%",
+                height: "50px",
+                marginBottom: "5px",
+              }}
+              onClick={() => {
+                alert("first is being clicked");
+              }}
+            ></div>
+            <div
+              style={{
+                background: "red",
+                width: "100%",
+                height: "50px",
+                marginBottom: "5px",
+              }}
+            ></div>
+            <div
+              style={{
+                background: "red",
+                width: "100%",
+                height: "50px",
+                marginBottom: "5px",
+              }}
+            ></div>
+            <div
+              style={{
+                background: "red",
+                width: "100%",
+                height: "50px",
+                marginBottom: "5px",
+              }}
+            ></div>
+          </div>
+        </div> */}
+        <div
+          id="parents div"
+          style={{
+            width: "100%",
+            height: "100vh",
+            background: "white",
+          }}
+        >
+          <NewmorphismBox />
+        </div>
+        <div
+          id="parents div"
+          style={{
+            width: "100%",
+            height: "100vh",
+            background: "white",
+          }}
+        >
+          <Stack direction="column" spacing={2}>
+            <div>
+              <Button variant="outlined" onClick={handleClickOpen}>
+                Show common dialog
+              </Button>
+              <CommonDialog
+                onClose={handleClose}
+                // src={
+                //   "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+                // }
+                open={open}
+                title={"Where is bby"}
+                content={"I want my bbyyy"}
+                primaryActionText="Confirm"
+                secondaryActionText="Cancel"
+                //primaryAction = {//can pass action here}
+              />
+            </div>
+            <div>
+              <Button variant="outlined" onClick={handleClickOpenDialogPicture}>
+                Show common dialog with pictures
+              </Button>
+              <CommonDialog
+                onClose={handleCloseDialogPicture}
+                src={
+                  "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+                }
+                open={openDialogPicture}
+                title={"Where picture is this"}
+                content={"will need to use my use memo"}
+                primaryActionText="Confirm"
+                secondaryActionText="Cancel"
+              />
+            </div>
+            <div>
+              <Button variant="outlined" onClick={handleClickOpenDialogOne}>
+                Show common dialog 1 action
+              </Button>
+              <CommonDialog
+                onClose={handleCloseDialogOne}
+                src={
+                  "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+                }
+                open={openDialogOne}
+                title={"Where picture is this"}
+                content={"will need to use my use memo"}
+                secondaryActionText="Close"
+              />
+            </div>
+          </Stack>
         </div>
       </div>
-    </div>
+    </AppTheme>
   );
 };
 
